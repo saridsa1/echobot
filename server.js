e5d59c10166ab113f8f66864d709a4797506325d
satyasuman <s.satya.suman@gmail.com>
2017-02-02 21:57:48 +0530

    Firebase integration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var restify = require('restify');
 var builder = require('botbuilder');
+var admin = require("firebase-admin");
 
 //=========================================================
 // Bot Setup
@@ -14,17 +15,39 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: "1dd05224-d740-4b09-88ee-407088fb99af",
     appPassword: "pbxzzeM06j1Q9bXsLwHKycC"
 });
+
+admin.initializeApp({
+    credential: admin.credential.cert("probot-d140b-firebase-adminsdk-97nyw-00a41cb203.json"),
+    databaseURL: "https://probot-d140b.firebaseio.com"
+});
+
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 var MobilityData = ["I have no problems in walking about", "I have some problems in walking about", "I am confined to Bed"];
 var SelfCareData = ["I have no problems with self care", "I have some problems washing or dressing myself", "I am unable to wash or dress myself"];
 
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+
+        admin.database().ref('/users'+message.user.name).set(message.address);
+
+        var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos. %s", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
 
 bot.dialog('/', [
     function (session) {
@@ -46,6 +69,15 @@ bot.dialog('/selfcare', [
         session.send("ok");
     }
 ]);
+
+server.get(/\/assets\/?.*/, restify.serveStatic({
+    directory: __dirname
+}));
+
+server.get(/\/?.*/, restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
 
 server.get('/', restify.serveStatic({
     directory: __dirname,
