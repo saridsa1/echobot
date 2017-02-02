5ba22d58feffe8db43c11bcb3728bdf5795cd0da
satyasuman <s.satya.suman@gmail.com>
2017-02-02 15:34:25 +0530

    added app ID and app password

--- server.js
+++ server.js
@@ -1,32 +1,62 @@
+/**
+ * Created by satyasumansaridae on 2/2/17.
+ */
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    appId: "1dd05224-d740-4b09-88ee-407088fb99af",
-    appSecret: "pbxzzeM06j1Q9bXsLwHKycC"
-};
-
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-
-// Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "1dd05224-d740-4b09-88ee-407088fb99af",
+    appPassword: "pbxzzeM06j1Q9bXsLwHKycC"
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+/**
+ *
+ */
+var salesData = {
+    "west": {
+        units: 200,
+        total: "$6,000"
+    },
+    "central": {
+        units: 100,
+        total: "$3,000"
+    },
+    "east": {
+        units: 300,
+        total: "$9,000"
+    }
+};
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.choice(session, "Which region would you like sales for?", salesData);
+    },
+    function (session, results) {
+        if (results.response) {
+            var region = salesData[results.response.entity];
+            session.send("We sold %(units)d units for a total of %(total)s.", region);
+        } else {
+            session.send("ok");
+        }
+    }
+]);
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
